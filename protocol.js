ef205a604f23507dd60de2705797d94938da83ed
KosM <admin@siriuspool.net>
2020-04-05 00:38:22 +0300

    return error to server , for basic commands

--- protocol.js
+++ protocol.js
@@ -1,19 +1,19 @@
 const Protocol = {
     Connected: "100",
     Connecting: "101",
-    DisConnect: "102",    
-    DO_REBOOT: "reboot_now",
-    DO_SHUTDOWN: "shut_down_now" ,
-    GET_UP_TIME: "up_time",
-    GET_HOST_NAME: "get_hostname",
-    GET_OS: "get_os" ,
-    GET_HDD: "get_hdd",
-    ANS_UPTIME: "up_time",
-    ANS_HOST_NAME: "host" ,
-    ANS_OS: "os",
-    ANS_HDD :"hdd_specs",
-    ERROR :{
-        INVALID_ID:"-10"
+    DisConnect: "102",
+    DO_REBOOT: "-1",
+    DO_SHUTDOWN: "0",
+    GET_UP_TIME: "200",
+    GET_HOST_NAME: "201",
+    GET_OS: "202",
+    GET_HDD: "203",
+    ANS_UPTIME: "300",
+    ANS_HOST_NAME: "301",
+    ANS_OS: "302",
+    ANS_HDD: "303",
+    ERROR: {
+        INVALID_ID: "-10"
     }
 }
 module.exports = Protocol;
